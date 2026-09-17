af456fd688b0d9a2f7b460f7d50c01802edf742c
Patrick Njiru <patricknjiru93@gmail.com>
2026-09-17 19:12:55 +0300

    feat: Connect my frontend to display through express server using express static so I only run one server

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,35 +1,15 @@
-import express, { type Express, type NextFunction, type Request, type Response } from "express";
+import express, { type Express } from "express";
+import { fileURLToPath } from "url";
 
 const app: Express = express();
 const PORT: number = 3000;
+const webDistPath = fileURLToPath(new URL("../../web/dist", import.meta.url));
 
+app.listen(PORT, () => {
+	console.log(`TODO List app listening on port ${PORT}`);
+});
 
-
-
-app.use("/", express.static("../../web/"))
-
-
-
-
-
-
-
-
-// // load middleware
-// app.all("/dashboard", (_req: Request, _res: Response, next: NextFunction) => {
-// 	console.log("Accessing a page requiring authentication...");
-// 	next();
-// });
-
-// app.get("/", (_req: Request, res: Response) => {
-// 	res.send("Hello world");
-// });
-
-// app.listen(PORT, () => {
-// 	console.log(`TODO List app listening on port ${PORT}`);
-// });
-
-// app.use("/static", express.static("public"));
+app.use("/", express.static(webDistPath));
 
 // app.use((_req: Request, res: Response, _next: NextFunction) => {
 // 	res.status(404).send("Sorry can't find that");
